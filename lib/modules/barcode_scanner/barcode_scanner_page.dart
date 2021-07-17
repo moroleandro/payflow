23e241eaa2d27be08a7f43e0c81b9fa18fa9259b
Leandro Moro <leandormoro1008@gmail.com>
2021-07-17 15:38:25 -0300

    Add page scan boleto

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,56 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_controller.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_status.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widtgets/label_button/label_button.dart';
-import 'package:payflow/shared/widtgets/divider_vertical/divider_vertical_widget.dart';
+import 'package:payflow/shared/widtgets/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:payflow/shared/widtgets/set_label_buttons/set_label_buttons.dart';
 
-class BarcodeScannerPage  extends StatefulWidget {
-  const BarcodeScannerPage({ Key? key }) : super(key: key);
+class BarcodeScannerPage extends StatefulWidget {
+  BarcodeScannerPage({Key? key}) : super(key: key);
 
   @override
   _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
 }
 
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  final controller = BarcodeScannerController();
+
+  @override
+  void initState() {
+    controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if (controller.status.hasBarcode) {
+        Navigator.pushReplacementNamed(context, "/insert_boleto",
+            arguments: controller.status.barcode);
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Escaneie o código de barras do boleto",
-          style: TextStyles.buttonBackground,  
-        ),
-        leading: BackButton(color: AppColors.background),
-      ),
-      body: Column(
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: Stack(
         children: [
-          Expanded(
-            flex: 2,
-            child: Container(color: Colors.transparent)
+          ValueListenableBuilder<BarcodeScannerStatus>(
+              valueListenable: controller.statusNotifier,
+              builder: (_, status, __) {
+                if (status.showCamera) {
+                  return Container(
+                    color: Colors.blue,
+                    child: controller.cameraController!.buildPreview(),
+                  );
+                } else {
+                  return Container();
+                }
+              }),
+          RotatedBox(
+            quarterTurns: 1,
+            child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  backgroundColor: Colors.black,
+                  centerTitle: true,
+                  title: Text(
+                    "Escaneie o código de barras do boleto",
+                    style: TextStyles.buttonBackground,
+                  ),
+                  leading: BackButton(
+                    color: AppColors.background,
+                  ),
+                ),
+                body: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.black,
+                      ),
+                    )
+                  ],
+                ),
+                bottomNavigationBar: SetLabelButtons(
+                  labelPrimary: "Inserir código do boleto",
+                  onTapPrimary: () {
+                    controller.status = BarcodeScannerStatus.error("Error");
+                  },
+                  labelSecondary: "Adicionar da galeria",
+                  onTapSecondary: controller.scanWithImagePicker,
+                )),
           ),
-          Expanded(
-            child: Container(color: Colors.black)
-          ),
+          ValueListenableBuilder<BarcodeScannerStatus>(
+              valueListenable: controller.statusNotifier,
+              builder: (_, status, __) {
+                if (status.hasError) {
+                  return Align(
+                      alignment: Alignment.bottomLeft,
+                      child: BottomSheetWidget(
+                          labelPrimary: "Escanear novamente",
+                          onTapPrimary: () {
+                            controller.scanWithCamera();
+                          },
+                          labelSecondary: "Digitar código",
+                          onTapSecondary: () {},
+                          title:
+                              "Não foi possível identificar um código de barras.",
+                          subtitle:
+                              "Tente escanear novamente ou digite o código do seu boleto."));
+                } else {
+                  return Container();
+                }
+              }),
         ],
       ),
-      bottomNavigationBar: 
-        Row(children: [
-          Expanded(
-            child: LabelButton(
-              label: "Inserir código do boleto",
-              onPressed: (){}
-            ),
-          ),
-          DividerVerticalWidget(height: 56),
-          Expanded(
-            child: LabelButton(
-              label: "Adicionar da galeria",
-              onPressed: (){}
-            ),
-          ),
-        ],
-      )
     );
   }
 }
---
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InserBoletoPage extends StatelessWidget {
+  const InserBoletoPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Insert boleto"))      
+    );
+  }
+}
